7dc40f6514482afa2bede06f9f6061e33010b700
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-02-08 19:00:31 +0330

    completing Style
    completing Style

---
+++ myJs.js
@@ -0,0 +1,7 @@
+const uidInput = document.querySelector(".userNameSection input"),
+      copyIcon = document.querySelector(".userNameSection .material-symbols-rounded"),
+      rangUidInput = document.querySelector(".rangeSlider input"),
+      uidSliderNo = document.querySelector(".rangeSlider .rangeNoUsername"),
+      btnGenerateUserName = document.querySelector(".btnGenerateUserName");
+
+// ======================================
