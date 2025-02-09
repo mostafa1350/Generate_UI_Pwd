f1464fc1343ecafb806b95f5c1c77376b774f613
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-02-09 12:24:27 +0330

    improve for generate uid
    improve for generate uid

--- myJs.js
+++ myJs.js
@@ -28,20 +28,21 @@ const makeUid = ()=>{
     console.log("chars Length =>" + charsForMakeUid.length);
    let newUidContent = uidInput.value ;
    let newLenghtUid = parseInt(uidInput.length);
+   console.log("inputt Length => "+ newUidContent.length);
 
-   if(newLenghtUid==0){
-    for (let index = 0; index < rangUidInput.value; index++) {
-        let randNoUid = Math.floor(Math.random()*charsForMakeUid.length);
-        newUidContent +=charsForMakeUid[randNoUid];        
-       }
-       console.log(newUidContent);
-       uidInput.value = newUidContent;
-   }
-   else if(newLenghtUid>uidInput.length){
-    
-   }
+//    if(newLenghtUid===0){
+//     for (let index = 0; index < rangUidInput.value; index++) {
+//         let randNoUid = Math.floor(Math.random()*charsForMakeUid.length);
+//         newUidContent +=charsForMakeUid[randNoUid];        
+//        }
+//        console.log(newUidContent);
+//        uidInput.value = newUidContent;
+//    }
+//    else if(newLenghtUid>uidInput.length){
+
+//    }
 }
-// =============== btn Event(CLICK) =============
+// =============== btn Event UID(CLICK) =============
 
 btnGenerateUserName.addEventListener("click",makeUid);
 // =============== COPY UID =============
@@ -64,7 +65,7 @@ const makePass = ()=>{
    pwdInput.value = newPwd;
 }
 
-// ============ btn Event(CLICK) ================
+// ============ btn Event PWD (CLICK) ================
 
 btnGeneratePwd.addEventListener("click",makePass);
 
